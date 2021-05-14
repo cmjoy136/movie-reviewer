6900e9cbc09ca7c0d688b6492fb32f26b99ecf72
Chris <cmjoy136@gmail.com>
2021-05-14 03:47:22 -0400

    changed whitenoise STATFILES_STORAGE variable to be inline with the most current whitenoise docs

--- movie_reviewer/settings.py
+++ movie_reviewer/settings.py
@@ -131,4 +131,4 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
